0741da38326e87516e7df16ed65f9cb7155c9f6d
FreeSpace <gejianquan@139.com>
2019-09-08 11:50:17 +0800

    Update SumCal1.cpp

--- SumCal1.cpp
+++ SumCal1.cpp
@@ -1,6 +1,22 @@
 #include "SumCal1.hpp"
 #include  <iostream>
 
+/***
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+***/
 std::vector<int> SumCal1::twoSum(std::vector<int>& nums, int target)
 {
 	
@@ -24,6 +40,23 @@ std::vector<int> SumCal1::twoSum(std::vector<int>& nums, int target)
 	return resNums;
 }
 
+/***
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+***/
 ListNode* SumCal1::addTwoNumbers(ListNode* l1, ListNode* l2)
 {
 	ListNode* l3 = NULL;
