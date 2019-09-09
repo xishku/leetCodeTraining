8456423f36df1f446159c245490fee23469f351c
FreeSpace <gejianquan@139.com>
2019-09-09 19:52:40 +0800

    Update SumCal1Test.cpp

--- test/SumCal1Test.cpp
+++ test/SumCal1Test.cpp
@@ -25,13 +25,74 @@ TEST_F(SumCal1Test, Target35ElementShouldBe3and7) {
 	}
 }
 
-TEST_F(SumCal1Test, ShouldReduceBookCountOnBorrow) {
+TEST_F(SumCal1Test, BigNumAdding7add8shouldBe15) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
 
+	ListNode l1(7);
+	ListNode l2(8);
+
+	ListNode* l3 = sCal1.addTwoNumbers(&l1, &l2);
+	ListNode* curNode = l3;
+	for (int i = 0; ; ++i)
+	{
+		if (NULL != curNode)
+		{
+			if (0 == i)
+			{
+				EXPECT_EQ(5, curNode->val);
+			}
+			else
+			{
+				EXPECT_EQ(1, curNode->val);
+			}
+			//std::cout << "print sum: " << curNode->val << std::endl;
+			curNode = curNode->next;
+		}
+		else
+		{
+			break;
+		}
+	}
 }
 
-TEST_F(SumCal1Test, ShouldThrowExceptionWhenNoBookIsAvailable) {
+TEST_F(SumCal1Test, lengthOfLongestSubstringabcabcbb) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
+	EXPECT_EQ(6, sCal1.lengthOfLongestSubstring("abcabcbbcdefg"));
 }
+
+TEST_F(SumCal1Test, lengthOfLongestSubstringbbb) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+
+	EXPECT_EQ(1, sCal1.lengthOfLongestSubstring("bbb"));
+}
+
+
+
+TEST_F(SumCal1Test, lengthOfLongestSubstringpwwkew) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+
+	EXPECT_EQ(3, sCal1.lengthOfLongestSubstring("pwwkew"));
+}
+
+TEST_F(SumCal1Test, lengthOfLongestSubstringEmptyStr) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+
+	EXPECT_EQ(0, sCal1.lengthOfLongestSubstring(""));
+}
+
+
+TEST_F(SumCal1Test, lengthOfLongestSubstringSpaceStr) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+
+	EXPECT_EQ(1, sCal1.lengthOfLongestSubstring(" "));
+}
+
+
+
+
