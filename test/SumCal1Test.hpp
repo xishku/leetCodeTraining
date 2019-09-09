270a84a8c427293a6cc900ae29046b8d718fbb97
FreeSpace <gejianquan@139.com>
2019-09-09 17:27:27 +0800

    Create SumCal1Test.hpp

---
+++ test/SumCal1Test.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "pch.h"
+#include "../leetCodeTraining/SumCal1.hpp"
+
+class SumCal1Test :
+	public testing::Test
+{
+protected:
+	virtual void SetUp() {
+		
+	}
+
+	virtual void TearDown() {
+
+	}
+
+	SumCal1 sCal1;
+
+};
+
+
+
+
