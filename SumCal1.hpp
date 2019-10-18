b497503f7348c0dbd51ffc178bca551ebefcd2a7
FreeSpace <gejianquan@139.com>
2019-10-18 10:14:28 +0800

    Update SumCal1.hpp

--- SumCal1.hpp
+++ SumCal1.hpp
@@ -2,6 +2,7 @@
 #include <string>
 
 using namespace std;
+
 struct ListNode {
 	int val;
 	ListNode* next;
@@ -12,12 +13,22 @@ struct ListNode {
 class SumCal1
 {
 public:
+	SumCal1(int inputNum) :num(inputNum) {};
 	std::vector<int> twoSum(std::vector<int>& nums, int target);
 	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
 	int lengthOfLongestSubstring(std::string s);
 	int lengthOfLongestSubstringLowSpeed(std::string s);
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
 	int integerReverse(int x);
+
+	//int operator""();
+	int game(vector<int>& guess, vector<int>& answer);
+	int maximumProduct(vector<int>& nums);
+	double myPow(double x, int n);
+	double myPowRecur(double x, int n);
+	
+private:
+	int num;
 };
 
 
