76d8696b4bc405188135a10a4e4924731db1c705
FreeSpace <gejianquan@139.com>
2020-10-14 10:30:30 +0800

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,46 +1,40 @@
 class Trie {
 private:
-    bool isEnd = false;
+    bool isEnd;
     Trie* next[26];
-
 public:
     Trie() {
         isEnd = false;
-        memset( next, 0, sizeof(next));
+        memset(next, 0, sizeof(next));
     }
-
+    
     void insert(string word) {
         Trie* node = this;
-        for ( char ch: word) {
-            if ( node->next[ ch - 'a' ] == NULL) {
-                node->next[ ch - 'a' ] = new Trie();
+        for (char c : word) {
+            if (node->next[c-'a'] == NULL) {
+                node->next[c-'a'] = new Trie();
             }
-
-            node = node->next[ ch - 'a' ];
+            node = node->next[c-'a'];
         }
-
         node->isEnd = true;
     }
-
+    
     bool search(string word) {
         Trie* node = this;
-        for ( char ch : word ) {
-            if (node-next[ ch - 'a' ] != NULL) {
-                node = node->next[ ch - 'a'];
-            } else {
+        for (char c : word) {
+            node = node->next[c - 'a'];
+            if (node == NULL) {
                 return false;
             }
         }
         return node->isEnd;
     }
-
+    
     bool startsWith(string prefix) {
         Trie* node = this;
-        for ( char ch : prefix ) {
-            if (node->next[ ch - 'a'] != NULL) {
-                node = node->next[ ch - 'a'];
-            }
-            else {
+        for (char c : prefix) {
+            node = node->next[c-'a'];
+            if (node == NULL) {
                 return false;
             }
         }
