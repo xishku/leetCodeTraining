5abb937eabfcd8147d67dc050bbfd80daeac1958
FreeSpace <gejianquan@139.com>
2021-04-02 15:43:30 +0800

    Update note.mk

--- xiaosi/note.mk
+++ xiaosi/note.mk
@@ -2,6 +2,7 @@
 1.测试代码实际修改较少；
 2.主要还是把Template代码调通了；非常抱歉，代码的修改可能已经离你的思路有点远了。但原逻辑没有修改，也没有验证和调试。
 
+
 代码或者说设计问题；
 1.friend class Queue<E>; //Queue是QueueNode的友元类，可以访问QueueNode的所有成员
 Queue和QueueNode的耦合应该是单向的，Queue不应该是QueueNode的友元；QueueNode是一种结构，应该可以放到不同的容器中，如自己的Queue，或者vector等等
@@ -10,4 +11,5 @@ Queue和QueueNode的耦合应该是单向的，Queue不应该是QueueNode的友�
 4.光靠返回值不能进行函数的重载；
 		QueueNode<E>* Head() { return head_; }
 		const QueueNode<E>* Head() const { return head_; } //为什么写两个 Head()? const？
+5.代码要模块化，小范围就测试好，再扩大范围，不同的逻辑不要放到一起，所谓高内聚低耦合。
    
